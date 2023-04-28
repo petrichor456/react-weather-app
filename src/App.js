9f25e06e2e42a7b4dd85883cc1954577267d7fd3
Mhairi Lyell <mhairi.long@gmail.com>
2023-04-28 10:58:26 +0100

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Mhairi Lyell and is open-sourced on{" "}
-        <a
-          href="https://github.com/petrichor456/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Mhairi Lyell and is open-sourced on{" "}
+          <a
+            href="https://github.com/petrichor456/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
